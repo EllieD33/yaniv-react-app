c883964edc281166dca434dd0ae811932a134b19
Ellie D <120728802+EllieD33@users.noreply.github.com>
2024-04-25 07:53:28 +0100

    finishes styling rules page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,13 @@ import Button from "../components/Button-large.js";
 const Home = () => {
     return (
         <div className="App-index">
-            <h1>Let's play Yaniv!</h1>
-            <a href="/pages/rules"><Button text="Learn how to play" /></a>
-            <a href="/pages/scorecard"><Button text="Take me to the scorecard" /></a>
+            <header className="Home-header">
+                <h1>Let's play Yaniv!</h1>
+            </header>
+            <nav>
+                <a href="/pages/rules"><Button text="Learn how to play" /></a>
+                <a href="/pages/scorecard"><Button text="Take me to the scorecard" /></a>
+            </nav>
         </div>
     );
 };
