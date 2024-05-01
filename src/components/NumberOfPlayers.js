ab83af62c220c65c33c37ad3f81764e211574877
Ellie D <120728802+EllieD33@users.noreply.github.com>
2024-05-01 19:42:45 +0100

    adds styling to dropdown

--- src/components/NumberOfPlayers.js
+++ src/components/NumberOfPlayers.js
@@ -4,19 +4,10 @@ import './NumberOfPlayers.css'
 
 function NumberOfPlayers ({ onChange }) {
     const [numberOfPlayers, setNumberOfPlayers] = useState("");
-    const [hoveredOption, setHoveredOption] = useState(null);
 
     const handleChange = (event) => {
         const { value } = event.target;
         setNumberOfPlayers(parseInt(value));
-    };
-
-    const handleMouseOver = (index) => {
-        setHoveredOption(index);
-    };
-
-    const handleMouseOut = () => {
-        setHoveredOption(null);
     };
 
     const handleClick = () => {
@@ -35,27 +26,23 @@ function NumberOfPlayers ({ onChange }) {
                 </div>
                 <div>
                     <div className="centre-container-items">
-                        <select 
-                            className="dropdown" 
-                            name="numberOfPlayers" 
-                            id="numberOfPlayers"
-                            onChange={handleChange}
-                            >
-                            {[2, 3, 4, 5, 6, 7, 8].map((option, index) => (
-                                <option
-                                    key={option}
-                                    value={option}
-                                    onMouseOver={() => handleMouseOver(index)}
-                                    onMouseOut={handleMouseOut}
-                                    style={{
-                                        backgroundColor: hoveredOption === index ? 'var(--secondary)' : 'var(--light)',
-                                        color: hoveredOption === index ? 'var(--secondary)' : 'var(--primary)'
-                                    }}
-                                    >
-                                        {option}
-                                    </option>
-                            ))}
-                        </select>
+                        <div className="select">
+                            <select
+                                className="dropdown"
+                                name="numberOfPlayers"
+                                id="numberOfPlayers"
+                                onChange={handleChange}
+                                >
+                                {[2, 3, 4, 5, 6, 7, 8].map((option, index) => (
+                                    <option
+                                        key={index}
+                                        value={option}
+                                        >
+                                            {option}
+                                        </option>
+                                ))}
+                            </select>
+                        </div>
                         <Button text="Add" onClick={handleClick}/>
                     </div>
                 </div>
