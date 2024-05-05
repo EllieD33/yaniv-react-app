853312fd9ff39ed871528e3c9921c2a085edea63
Ellie D <120728802+EllieD33@users.noreply.github.com>
2024-05-05 08:53:33 +0100

    adds responsive burger menu functionality to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,40 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './NavBar.css'
 
 const NavBar = () =>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(prevState => !prevState)
+    };
+
     return (
-    <ul className="NavBar">
-        <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/pages/rules">Rules</Link>
-        </li>
-        <li>
-            <Link to="/pages/scoreboard">Scoreboard</Link>
-        </li>
-    </ul>
+        <nav className='NavBar'>
+            <div className="NavBar-links">
+                <ul className="NavBar-menu">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/pages/rules">Rules</Link></li>
+                    <li><Link to="/pages/scoreboard">Scoreboard</Link></li>
+                </ul>
+            </div>
+            <div className='Burger-menu-container'>
+                <div className="NavBar-burger" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+                {isOpen && (
+                    <div className="Burger-menu">
+                        <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/pages/rules">Rules</Link></li>
+                        <li><Link to="/pages/scoreboard">Scoreboard</Link></li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </nav>
     );
 }
 export default NavBar;
